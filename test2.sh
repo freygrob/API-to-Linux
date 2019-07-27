1b97298ba7bc1f7abc3403bec726882f4961939d
Fabien Reygrobellet <fabien@reygrobellet.com>
2019-07-27 22:16:36 +0200

    Create test2.sh

---
+++ test2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "test2: start"
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+sleep 30
+echo $(date)
+
+echo "test2: ok"
